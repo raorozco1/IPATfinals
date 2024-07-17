9c9edcf4ea5d75d8490af18bef96c639d065ec34
raorozco1 <orozco.ra.bsinfotech>
2024-07-17 12:46:00 +0800

    first commit

---
+++ backend/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer 
+
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+# Create your views here.
+
